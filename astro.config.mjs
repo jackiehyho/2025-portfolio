1bb6b5dca4dfb1cfdbda7263d579d0814556ec39
jackiehyho <jackie.hy.ho@gmail.com>
2025-03-14 13:58:59 -0700

    install astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     devToolbar: {
         enabled: false
-      }
+      },
+    integrations: [(await import("astro-compress")).default()]
+
 });
